73df57f8c9a6f7823036dfc4b03374fed46dd5e2
Hariprasath R <balaprasad24@gmail.com>
2021-06-06 16:20:31 +0530

    Spring Boot Redis Playground

---
+++ src/main/java/in/hp/java/springdataredisstarter/SpringDataRedisStarterApplication.java
@@ -0,0 +1,13 @@
+package in.hp.java.springdataredisstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRedisStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRedisStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/hp/java/springdataredisstarter/SpringDataRedisStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package in.hp.java.springdataredisstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
