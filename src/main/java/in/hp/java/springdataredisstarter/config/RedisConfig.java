a8e0346981aead3746eb599e9a45094a5f498afa
Hariprasath R <balaprasad24@gmail.com>
2021-06-06 16:38:18 +0530

    Configured Redis Connectivity

---
+++ src/main/java/in/hp/java/springdataredisstarter/config/RedisConfig.java
@@ -0,0 +1,17 @@
+package in.hp.java.springdataredisstarter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+}
